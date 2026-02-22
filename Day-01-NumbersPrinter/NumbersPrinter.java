30bbd38f5262feb23be8e65aa2833861eb329c62
John-Akech <johnakec12@gmail.com>
2026-02-22 18:43:20 +0200

    Day 01: NumbersPrinter (loops)

---
+++ Day-01-NumbersPrinter/NumbersPrinter.java
@@ -0,0 +1,20 @@
+public class NumbersPrinter {
+    public static void main(String[] args) {
+        // Printing numbers using loops
+        
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i + " ");
+        }
+
+        // int i = 1;
+        // while(i <= 100) {
+        //     System.err.println(i + " ");
+        //     i++;
+        // }
+
+        //  do {
+        //     System.out.println(i + " ");
+        //     i++;
+        // } while(i <= 100);
+    }
+}
