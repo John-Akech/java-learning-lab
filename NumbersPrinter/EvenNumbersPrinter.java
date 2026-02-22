1e8c83a3a89336d261f12a6c7b0ae505209639f6
John-Akech <johnakec12@gmail.com>
2026-02-22 18:56:35 +0200

    NumbersPrinter (loops)

---
+++ NumbersPrinter/EvenNumbersPrinter.java
@@ -0,0 +1,14 @@
+public class EvenNumbersPrinter {
+    public static void main(String[] args) {
+        // Even numbers printer
+        for(int i = 1; i <= 100; i++){
+            if(i%2 == 0){
+                System.out.println(i + " Is an even number.");
+            } else{
+                continue;
+            }
+        }
+        System.out.println("You have come to an end.");
+    }
+    
+}
